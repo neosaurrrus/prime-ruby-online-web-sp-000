a684ce6461be36c2c3867fa0920d879e41ed0565
Lukie Kang <lukiek+f@gmail.com>
2019-05-27 20:08:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   range = [*2..number]
   if number == 1 || number == 2
     return true
-  elsif number.even?
+  elsif number.even? || number < 1
     return false
   else
     isPrime = true
