0babe7e5b01423a212c3231916ad9875e1f30dee
Lukie Kang <lukiek+f@gmail.com>
2019-05-27 19:56:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(number)
   range = [*2..number]
-  if number < 1
-    return false
-  elsif number == 1 || number == 2
+  if number == 1 || number == 2
     return true
   elsif number.even?
     return false
