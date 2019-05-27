a86f3b76d083bd125a87ed378d22ec05d78ebc00
Lukie Kang <lukiek+f@gmail.com>
2019-05-27 21:00:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  if number == 2
+    return true
+  elsif number.even? || number <= 1
+    return false
+  else
+    range = [*2..number]
+    range.each do | num |
+      if (number % num) == 0
+       return false
+      end
+      if num > number / 2
+        return true
+      end
+    end
+  end
+end
